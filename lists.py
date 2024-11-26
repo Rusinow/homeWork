d428c9bab9c23cf907245b627498c305cc9425dc
Aleksandr <rusinow89@gmail.com>
2024-11-26 18:53:24 +0300

    lesson

---
+++ lists.py
@@ -0,0 +1,10 @@
+food = ['apple', 'coconut', 'banana']
+food.append(True)
+print(food)
+food.extend(['string', 2])
+print(food)
+food.remove('apple')
+print(food)
+print('coconut' in food)
+print('coconut' not in food)
+print(food[0:2:2])
