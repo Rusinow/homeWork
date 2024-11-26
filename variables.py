f0c0e63a5ea7d22ad23ad2bd5e01d14f2ff7d50e
Aleksandr <rusinow89@gmail.com>
2024-11-26 14:56:33 +0300

    ready

---
+++ variables.py
@@ -0,0 +1,9 @@
+sum_homework = 12                   #Количество выполненных ДЗ
+sum_hours = 1.5                     #Количество затраченных часов
+course_name = 'Python'              #Название курса
+time_for_task = sum_hours / sum_homework
+#Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+print('Course:', course_name, ',','total tasks:', sum_homework, ',',
+      'hours spent:', sum_hours, ',', 'average execution time', time_for_task)
+print('Курс:', course_name, ',', 'всего задач:', sum_homework, ',',
+      'затрачено часов:', sum_hours, ',' 'среднее время выполнения', time_for_task)
