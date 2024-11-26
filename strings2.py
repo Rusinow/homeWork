b83627bfec63a4fe38f72a0bc042aa68d095b084
Aleksandr <rusinow89@gmail.com>
2024-11-26 16:12:55 +0300

    new

---
+++ strings2.py
@@ -0,0 +1,7 @@
+# name = input('Enter your name: ')
+#current_year = 2024
+#date_of_birth = int(input('в каком году вы родились? '))
+#age = current_year - date_of_birth
+#print('Hello, ', name, '!')
+#print('В этом году вам',  age, 'лет')
+print('Привет, я строка в нижнем ригистре'.replace('Привет', 'пока'))
