db4d83d4b32d88cad7506f332b4804f811dc995e
Aleksandr <rusinow89@gmail.com>
2024-12-01 12:32:32 +0300

    ready1

--- module_2_2.py
+++ module_2_2.py
@@ -4,7 +4,7 @@ second = input('enter the second number: ')
 third = input('enter the third number: ')
 if first == second and first == third:
     print(3)
-elif first == second or first == third:
+elif first == second or second == third or first == third:
     print(2)
 else:
     print(0)
