1fd4b62c6c95adb6995034e18d84bbc733778c87
Aleksandr <rusinow89@gmail.com>
2024-12-02 09:10:41 +0300

    ready

---
+++ lessons/pep8.py
@@ -0,0 +1,7 @@
+name = input('Enter your name: ')
+x, y = 5, 2
+a = 2
+if 5 > 1:
+    print('ok')
+    if 1 >= 0:
+        print("i'm here")
---
+++ lessons/while.py
@@ -0,0 +1,9 @@
+while 1 > 0:
+    number = int(input('enter number: '))
+    if number % 2 == 0:
+        print('number is even')
+        continue
+    else:
+        print('number in not even')
+    print('mi forget')
+print('end')
