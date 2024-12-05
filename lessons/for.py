eb682abe9330255e59a10632265ee6f99eae127c
Aleksandr <rusinow89@gmail.com>
2024-12-05 14:42:34 +0300

    new_hw

---
+++ lessons/for.py
@@ -0,0 +1,7 @@
+for i in range(1, 11):   # start, stop, step
+    for j in range(1, 11):
+        print(f'{i} x {j} = {i*j}')
+
+dict_ = {'a': 1, 'b': 2, 'c': 3 }
+for i, k in dict_.items():
+    print(i, k)
---
+++ module_2_4.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+is_prime = True
+
+for i in numbers:
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break
+        is_prime = True
+    if i == 1:              #исключение, число не является ни простым, ни составным
+        continue            #по этому для него особое условие
+    elif is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+
+print('Primes: ', primes)
+print('Not primes: ', not_primes)
