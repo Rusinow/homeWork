f7160fd9920a864d269382b9a39e9e313bae06a3
Aleksandr <rusinow89@gmail.com>
2024-12-05 20:04:04 +0300

    new_hw

--- module_2_4.py
+++ module_2_4.py
@@ -1,11 +1,13 @@
+# Инициализация
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12, 13, 14, 15]
-primes = []
-not_primes = []
+primes, not_primes = [], []
 is_prime = True
 
+# Основной блок скрипта
+
 for i in numbers:
-    for j in range(2, i):
-        if i % j == 0:
+    for j in range(1, i):
+        if i % j == 0 and j != 1:
             is_prime = False
             break
         is_prime = True
