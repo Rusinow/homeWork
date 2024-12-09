d2da599cb4a1431e1207a8f9dcfd812c22da7ace
Sanek-spiner <rusinow89@gmail.com>
2024-12-09 18:34:23 +0300

    new_lesson

---
+++ lessons/module_3/func_with_params.py
@@ -0,0 +1,11 @@
+def func_with_params(a, b=2,c=None):
+    if c is None:
+        c = []
+        c.append(a)
+    print(c)
+
+
+func_with_params(3)
+func_with_params(5)
+func_with_params(5)
+func_with_params(5)
