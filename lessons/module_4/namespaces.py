f75d12492a73498e5158e3720d272eb02a6f0744
Aleksandr <rusinow89@gmail.com>
2024-12-27 11:22:57 +0300

    new_lesson

---
+++ lessons/module_4/namespaces.py
@@ -0,0 +1,22 @@
+from mod3 import *
+d = 7
+
+def square(x):
+    d = x ** 2
+    def even(x):
+        nonlocal d
+        d = x / 2
+        if d % 2 == 0:
+            print('четное')
+        else:
+            print('нечетное')
+    even(x)
+    return d
+
+a = 5
+b = square(4)
+print(a)
+print(b)
+
+
+
